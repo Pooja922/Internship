a23d58b3db42a4d9093f52842d5530bb2fa6a46f
Pooja <shettypooja2000@gmail.com>
2021-04-01 17:37:16 +0530

    Linking between pages

--- myfirstapp/components/Navbar.js
+++ myfirstapp/components/Navbar.js
@@ -1,12 +1,15 @@
+import React from "react";
+import Link from "next/link"
+
 const navbar=()=>{
     return(
         <nav>
             <div className="logo">
                 <h1>List</h1>
             </div>
-            <a>Home</a>
-            <a>About</a>
-            <a>Listing</a>
+            <Link href="/"><a>Home</a></Link>
+            <Link href="/about"><a>About</a></Link>
+            <Link href="/demo"><a>Demo Listing</a></Link>
         </nav>
     )
 }
